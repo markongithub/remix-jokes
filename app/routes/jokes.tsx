5c6575782caf0bd08febc42b7afcab14ee6aa82f
mark on github <markongithub@users.noreply.github.com>
2023-02-22 07:14:13 -0500

    Add the links export to jokes.tsx

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,4 +1,12 @@
+import type { LinksFunction } from "@remix-run/node";
+
 import { Outlet } from "@remix-run/react";
+
+import stylesUrl from "~/styles/jokes.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
 
 export default function JokesRoute() {
   return (
