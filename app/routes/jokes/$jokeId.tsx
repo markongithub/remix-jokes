4d2efbd80d622e94135f9ad2fe58013a438f6d95
mark on github <markongithub@users.noreply.github.com>
2023-02-23 08:47:53 -0500

    this proves I can do useLoaderData without copying straight from the tutorial

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,7 +1,17 @@
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  console.log("$jokeId.tsx params: " + params);
+  return params.jokeId;
+};
+
+
 export default function JokeRoute() {
+  const jokeId = useLoaderData<typeof loader>();
+
     return (
       <div>
-        <p>Here's your hilarious joke and I didn't consider jokeId at all:</p>
+        <p>Here's your hilarious joke although I guess you wanted joke {jokeId}:</p>
         <p>
           (This one is hardcoded in $jokeid.tsx.) Why don't you find hippopotamuses hiding in trees?
           They're really good at it.
